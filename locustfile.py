573ab9bd9c1e3c45d2db1eb2ddbd9d6b42539263
Layl <LaylConway@users.noreply.github.com>
2016-07-21 13:28:41 +0200

    Move style to separate stylesheet

--- locustfile.py
+++ locustfile.py
@@ -2,13 +2,21 @@ from random import randint
 from locust import HttpLocust, TaskSet, task
 
 class WebsiteTasks(TaskSet):
-    @task
+    @task(2)
     def index(self):
         self.client.get("/")
 
-    @task
+    @task(2)
     def number(self):
         self.client.get("/number/" + str(randint(0,9)))
+
+    @task(1)
+    def rows(self):
+        self.client.get("/rowtest")
+
+    @task(1)
+    def textfile(self):
+        self.client.get("/public/test.txt")
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
