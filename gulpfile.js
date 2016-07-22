2d8c4d629846d99256796add1de05b4af2543990
Layl <LaylConway@users.noreply.github.com>
2016-07-22 03:27:08 +0200

    Simplify sass with sass-module-importer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var moduleImporter = require('sass-module-importer');
 
 gulp.task('sass', function() {
     return gulp.src('./style/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({ importer: moduleImporter() }).on('error', sass.logError))
         .pipe(gulp.dest('./public/style'));
 });
 
